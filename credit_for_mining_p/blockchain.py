a6199d7ce307be70ace054c37a7748316b7a02ba
Brian Illes <brianrilles@gmail.com>
2019-07-11 13:48:15 -0700

    ^

--- credit_for_mining_p/blockchain.py
+++ credit_for_mining_p/blockchain.py
@@ -248,13 +248,13 @@ def mine():
     last_proof = last_block['proof']
 
     values = request.get_json()
-    submitted_proof = values.get('proof')
+    submitted_proof = values.get('proof', 'id')
 
     if blockchain.valid_proof(last_proof, submitted_proof):
         # We must receive a reward for finding the proof.
         # The sender is "0" to signify that this node has mine a new coin
         blockchain.new_transaction(
-            sender="0",
+            sender=submitted_proof.id,
             recipient=node_identifier,
             amount=1,
         )
